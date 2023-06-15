ad638767b6caf6f8cf4146813b6109ea74fdcd28
JamieJiang <hookttg@users.noreply.github.com>
2023-06-15 17:20:43 +0800

    hook

--- examples/hello/main.go
+++ examples/hello/main.go
@@ -4,7 +4,7 @@ import (
 	"log"
 	"net/url"
 
-	"github.com/zserge/lorca"
+	"github.com/hookttg/lorca"
 )
 
 func main() {
--- examples/stopwatch/main.go
+++ examples/stopwatch/main.go
@@ -7,7 +7,7 @@ import (
 	"sync/atomic"
 	"time"
 
-	"github.com/zserge/lorca"
+	"github.com/hookttg/lorca"
 )
 
 func main() {
